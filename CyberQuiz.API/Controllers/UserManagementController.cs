02aaf4ec6e602209e476f18f737d76063b2d3b3b
Amin Nazari <Aminnazari040@hotmail.com>
2026-03-02 10:38:36 +0100

    bugfixes

---
+++ CyberQuiz.API/Controllers/UserManagementController.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+
+namespace CyberQuiz.API.Controllers
+{
+
+
+    public class UserManagementController
+    {
+        private readonly SignInManager signInManager;
+
+        public UserManagementController(SignInManager SignInManager)
+        {
+            signInManager = SignInManager;
+        }
+    }
+}
