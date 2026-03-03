cce053d541d717c6eb137fef07ed20c8c101e1ad
Amin Nazari <Aminnazari040@hotmail.com>
2026-03-03 19:21:35 +0100

    (API) La till UserManagementController.cs som tar emot login och registrering, skapar token. API Program.cs - konfiguera API. Databas, identity och JWT. Appsetting.json- Databasadress och jemlig JWT nyckel. (UI). AuthService.cs - minnet som håller koll på token och userID Program.cs tar bort identity, registrerar AuthService.

--- CyberQuiz.API/Program.cs
+++ CyberQuiz.API/Program.cs
@@ -2,8 +2,11 @@ using CyberQuiz.API.Services;
 using CyberQuiz.BLL.Services;
 using CyberQuiz.DAL;
 using CyberQuiz.DAL.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;  
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,25 +31,66 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IQuizService, QuizService>();
 // När någon frågar efter IUserResultService får de en UserResultService tillbaka.
 builder.Services.AddScoped<IUserResultService, UserResultService>();
+// När någon frågar efter IUserService får de en UserService tillbaka.
+builder.Services.AddScoped<IUserService, UserService>(); 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+
+//Databas
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+
 builder.Services.AddIdentityCore<ApplicationUser>(options =>
 {
-    options.SignIn.RequireConfirmedAccount = true;
-    options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
+    options.SignIn.RequireConfirmedAccount = false;
 })
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
+    .AddEntityFrameworkStores<ApplicationDbContext>() // kpllar till databasen
+    .AddSignInManager() // registrerar SignINManager
+    .AddDefaultTokenProviders(); // flr lösenordåterställning
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
+
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//builder.Services.AddIdentityCore<ApplicationUser>(options =>
+//{
+//    options.SignIn.RequireConfirmedAccount = true;
+//    options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
+//})
+//    .AddEntityFrameworkStores<ApplicationDbContext>()
+//    .AddSignInManager()
+//    .AddDefaultTokenProviders();
+
+//builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+
+// JWT så att API förstår och godkänner tokens från UI
+var jwtKey = builder.Configuration["Jwt:Key"]
+    ?? throw new InvalidOperationException("JWT-nyckel saknas i appsettings.json");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 
 var app = builder.Build();
